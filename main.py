80716296b34641e2338150444efd5429bfd1e096
pranavshukla634-sys <pranavshukla634@gmail.com>
2025-11-05 14:42:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from telegram.ext import (
 # 🔒 HARD-CODED CONFIG
 # -----------------------------
 ADMIN_ID = 7370025284
-GROUP_ID = -1002621279973
+GROUP_ID = -5057716875
 BOT_TOKEN = "8458622801:AAFWZDxnB8ZGoQEtrljhuPGA8GHzghytpLU"
 HEALTH_URL = "https://pranav12.pythonanywhere.com/"
 # -----------------------------
@@ -939,3 +939,4 @@ if __name__ == "__main__":
     keep_alive_thread.start()
 
     main()
+
